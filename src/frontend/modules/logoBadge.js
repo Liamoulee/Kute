891160289b38166aab3181e366287c60af5f5248
NullDev (Shadow) <chris@nulldev.org>
2026-09-17 20:12:31 +0200

    more frontend fixes

--- src/frontend/modules/logoBadge.js
+++ src/frontend/modules/logoBadge.js
@@ -20,8 +20,9 @@ class LogoBadge {
                 position-anchor: --kute-main-logo;
                 width: calc(anchor-size(width) * 0.25);
                 aspect-ratio: 1;
-                left: calc(anchor(right) - anchor-size(width) * 0.22);
-                top: calc(anchor(bottom) - anchor-size(height) * 0.48);
+                left: calc((anchor(right) - anchor-size(width) * 0.22) - 15px);
+                top: calc((anchor(bottom) - anchor-size(height) * 0.48) - 10px);
+                transform: rotate(-25deg);
                 background: url(${logo}) center / contain no-repeat;
                 pointer-events: none;
             }`;
